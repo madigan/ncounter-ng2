f87b6a4d5a744e9b4fe109c1291628288875773d
John Lynn <john@silferein.com>
2017-09-02 18:50:42 -0700

    Add combatant widget.

--- src/app/combatant/combatant.component.ts
+++ src/app/combatant/combatant.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EncounterService } from '../encounter/encounter.service';
+import { Combatant } from './combatant.model';
+
 @Component({
   selector: 'app-combatant',
   templateUrl: './combatant.component.html',
   styleUrls: ['./combatant.component.css']
 })
 export class CombatantComponent implements OnInit {
+  public combatant:Combatant;
 
-  constructor() {
-    
-  }
+  constructor(private encounterService:EncounterService) { }
 
   ngOnInit() {
+    this.reset();
   }
 
+  addCombatant():void {
+    this.encounterService.getCurrentEncounter().add( this.combatant );
+    this.reset();
+  }
+
+  reset():void {
+    this.combatant = new Combatant("", 0);
+  }
 }
--- src/app/encounter/encounter.model.ts
+++ src/app/encounter/encounter.model.ts
@@ -6,7 +6,7 @@ export class Encounter {
 
   constructor(combatants:Combatant[]) {
     this._combatants = combatants;
-    this._combatants.sort((a, b) => b.initiative-a.initiative);
+    this.sort();
 
     this._currentIndex = 0;
   }
@@ -15,9 +15,18 @@ export class Encounter {
     this._currentIndex = (this._currentIndex + 1) % this._combatants.length;
   }
 
-  remove(combatant:Combatant): void {
+  remove(combatant:Combatant):void {
     this._combatants = this._combatants.filter( (c) => c != combatant );
     if(this._currentIndex >= this._combatants.length) this._currentIndex = 0;
+  }
+
+  add(combatant:Combatant):void {
+    this._combatants.push( combatant );
+    this.sort();
+  }
+
+  private sort():void {
+    this._combatants.sort((a, b) => b.initiative-a.initiative);
   }
 
   get currentCombatant():Combatant {
