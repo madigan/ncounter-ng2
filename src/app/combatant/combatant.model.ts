f58dae5cce148e4681effabdf99326ee6b45ddca
John Lynn <john@silferein.com>
2017-09-02 20:37:23 -0700

    Add health to combatants.

--- src/app/combatant/combatant.model.ts
+++ src/app/combatant/combatant.model.ts
@@ -1,9 +1,22 @@
 export class Combatant {
   name:string;
   initiative:number;
+  currentHP?:number;
+  maxHP?:number;
 
-  constructor(name:string, initiative:number) {
+  constructor(name:string, initiative:number, health:number=1) {
     this.name = name;
     this.initiative = initiative;
+    this.currentHP = health;
+    this.maxHP = health;
+  }
+
+  set health(health:number) {
+    this.currentHP = health;
+    this.maxHP = health;
+  }
+
+  get health():number {
+    return this.maxHP;
   }
 };
